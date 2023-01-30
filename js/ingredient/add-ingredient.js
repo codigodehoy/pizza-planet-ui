97b59a8fa7c068e565403516ce2031a6a3ea9c1c
David Cadena <david.cadena@hotmail.com>
2023-01-30 13:11:53 -0500

    add report view

--- js/ingredient/add-ingredient.js
+++ js/ingredient/add-ingredient.js
@@ -1,30 +1,28 @@
-function postIngredient(ingredient) {
 
-    fetch('http://127.0.0.1:5000/ingredient/', {
+async function postIngredient(ingredient) {
+    const response = await fetch('http://127.0.0.1:5000/ingredient/', {
         method: 'POST',
         body: JSON.stringify(ingredient),
         headers: {
             "Content-Type": "application/json; charset=utf-8",
         },
     })
-        .then(res => res.json())
-        .then(res => showNotification());
-
-
+    return response; 
 }
 
 /**
  * Get the form and submit it with fetch API
  */
-let ingredientForm = $("#ingredient-form");
-ingredientForm.submit(event => {
+const ingredientForm = $("#ingredient-form");
 
+ingredientForm.submit(async (event) => {
     let ingredient = getIngredientData();
-    postIngredient(ingredient);
-
+    await postIngredient(ingredient);
     event.preventDefault();
     event.currentTarget.reset();
 });
+
+
 
 /**
  * Gets the order data with JQuery
---
+++ js/report/report.js
@@ -0,0 +1,6 @@
+import { insertDataToTable } from '../assets/index.js'
+
+
+insertDataToTable("report", "#report-ingredient-template", "#report-ingredient");
+insertDataToTable("report/month", "#report-month-template", "#report-month");
+insertDataToTable("report/customers", "#report-customers-template", "#report-customers");
